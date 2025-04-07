2bc87a89ed53ada2423aa77b373247fc85c99c6a
114188_NicolasHerrera <114188@tecnicatura.frc.utn.edu.ar>
2025-04-06 21:32:16 -0300

    tarea realizada

--- src/main/java/ar/edu/utn/frc/tup/lciii/App.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/App.java
@@ -10,14 +10,16 @@ public class App
 {
     public static void main( String[] args )
     {
+       // System.out.println("--------------Ingrese tres numeros enteros-------------");
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
 
         System.out.println(a);
-        // Complete this line
-        // Complete this line
+        System.out.println(b);
+        System.out.println(c);
 
     }
 }
